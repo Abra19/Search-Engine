6d96a189691345e541e6fa596cef0e5bc93d8337
Abra19 <evabrarova@gmail.com>
2023-10-21 01:14:57 +0300

    add search function and prepare package

--- tests/test_search_engine.py
+++ tests/test_search_engine.py
@@ -1,16 +1,18 @@
 import pytest
 from search_engine.search_engine import search
 
+
 @pytest.fixture
-def docs(): 
+def docs():
     doc1 = "I can't shoot straight unless I've had a pint!"
     doc2 = "Don't shoot shoot shoot that thing at me."
     doc3 = "I'm your shooter."
     return [
-      {'id': 'doc1', 'text': doc1},
-      {'id': 'doc2', 'text': doc2},
-      {'id': 'doc3', 'text': doc3},
-  ]
+        {'id': 'doc1', 'text': doc1},
+        {'id': 'doc2', 'text': doc2},
+        {'id': 'doc3', 'text': doc3},
+    ]
+
 
 def test_search_positive(docs):
     expected = ['doc1', 'doc2']
