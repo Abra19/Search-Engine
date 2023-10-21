4efd9019c53601cd6b34aade73a7e5cb9578ce25
Abra19 <evabrarova@gmail.com>
2023-10-22 00:22:32 +0300

    clean text in search

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,11 +1,20 @@
 import re
 
+
 def clean_text(text):
     return re.sub(r'[^\w\s]', '', text)
+
 
 def search(docs, sample):
     if sample == '':
         return [doc['id'] for doc in docs]
     token = clean_text(sample)
-    cleaned_docs = map(lambda doc: {**doc, 'text': clean_text(doc['text'])}, docs)
-    return [doc['id'] for doc in cleaned_docs if token in doc['text'].split(' ')]
+    cleaned_docs = map(
+        lambda doc: {**doc, 'text': clean_text(doc['text'])},
+        docs
+    )
+    return [
+        doc['id']
+        for doc in cleaned_docs
+        if token in doc['text'].split(' ')
+    ]
